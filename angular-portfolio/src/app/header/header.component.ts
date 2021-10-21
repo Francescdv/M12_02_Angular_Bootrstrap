ffc9e7fc839d0df62c14bb0f981ed4da4ef14c44
francescdv <francescdv@gmail.com>
2021-10-21 12:43:49 +0200

    no message

--- angular-portfolio/src/app/header/header.component.ts
+++ angular-portfolio/src/app/header/header.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+  // Navbar shrink function
+  var navbarShrink = function () {
+      const navbarCollapsible = document.body.querySelector('#mainNav');
+      if (!navbarCollapsible) {
+          return;
+      }
+      if (window.scrollY === 0) {
+          navbarCollapsible.classList.remove('navbar-shrink')
+      } else {
+          navbarCollapsible.classList.add('navbar-shrink')
+      }
+
+  };
+
+  // Shrink the navbar
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener('scroll', navbarShrink);
+
+});
+
   }
 
 }
